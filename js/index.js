3f93e6935a78df17cf632add8f8521d1feca5023
Silas H. Rasch <silasrasch@gmail.com>
2023-12-06 14:49:16 +0100

    login :(

--- js/index.js
+++ js/index.js
@@ -1,10 +1,46 @@
-url=""
+api = "https://measurementapi.azurewebsites.net/api/Login"
 
+axios.defaults.headers.common["Authorization"] = 'Bearer' + localStorage.getItem('token')
 
 app = Vue.createApp({
-    data(){
+    data() {
         return{
-            Title: "KlimaKontrol"
+            Title: "Klimakontrol",
+            username: null,
+            password: null,
+            token: null
+        }
+    },
+    async created() {
+    },
+
+    methods: {
+        async login() {
+            console.log("Run")
+            url = "http://localhost:5034/api/login" + "?username=" + this.username + "&password=" + this.password
+
+            await axios.post(url).then(response => {
+                if (response.data.value.token) {
+                    this.token = response.data.value.token
+                    localStorage.setItem('token', this.token)
+                    window.location.href = "/index.html"
+                }
+            }).catch(error => {
+                console.log(error.message)
+            })
+
+            console.log(this.token)
+        },
+
+        logout() {
+            localStorage.removeItem('token')
+            this.token = null
+            window.location.href = "/index.html"
+
+        },
+
+        async isAuthenticated() {
+            
         }
     }
 })
