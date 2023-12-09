1b242b58bc59e466ca9bac421ba92f5dd4326e79
Silas H. Rasch <silasrasch@gmail.com>
2023-12-09 14:16:15 +0100

    log done

--- js/log.js
+++ js/log.js
@@ -1,5 +1,4 @@
-urlRoom = "https://measurementapi.azurewebsites.net/api/Rooms"
-urlMeasurement = "https://measurementapi.azurewebsites.net/api/Measurements"
+api = "https://measurementapi.azurewebsites.net/api/Measurements"
 
 /// TODO: PAGINATION
 
@@ -8,57 +7,33 @@ app = Vue.createApp({
         return {
             title: "Målingshistorik",
             measurements: [],
-            rooms: [],
-            institutionId: 1,
             page: 1,
             pageSize: 20,
         }
     },
 
     async created(){
-        //await this.getAllMeasurements()
-        //await this.getAllRooms()
         await this.getMeasurements()
     },
 
     methods: {
-        async getAllMeasurements() {
-            try{
-                const response = await axios.get(urlMeasurement)
-                this.measurements = await response.data
-            }
-            catch(ex){
-                alert(ex.message)
-            }
-        },
-
-        async getAllRooms(){
-            try{
-                const response = await axios.get(urlRoom)
-                this.rooms = await response.data
-            }
-            catch(ex){
-                alert(ex.message)
-            }
-        },
-
         async getMeasurements() {
-            url = urlMeasurement + "?page=" + this.page + "&amount=" + this.pageSize
+            url = api + "?page=" + this.page + "&amount=" + this.pageSize
             const response = await axios.get(url)
             this.measurements = await response.data
         },
 
-        nextPage() {
+        async nextPage() {
             if (this.measurements.length == this.pageSize) {
                 this.page += 1
-                this.getMeasurements()
+                await this.getMeasurements()
             }
         },
 
-        previousPage() {
+        async previousPage() {
             if (this.page > 1) {
                 this.page -= 1
-                this.getMeasurements()
+                await this.getMeasurements()
             }
         },
     }
